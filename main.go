a4ffa66c51951752e29fc0b0e2875186b1095d19
maximediet <maxime.diet76@gmail.com>
2023-03-23 02:54:11 +0100

    Add recursivity to watch every folders inside main folder etc...

--- main.go
+++ main.go
@@ -2,39 +2,61 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/fsnotify/fsnotify"
 )
 
+func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer watcher.Close()
 
-func main(){
-  watcher,err := fsnotify.NewWatcher()
-  if err!= nil {
-    fmt.Println(err)
-    return
-  }
-  defer watcher.Close()
-  
-  err = watcher.Add("/home/student07")
-  if err != nil {
-    fmt.Println(err)
-    return
-  } 
+	err = filepath.Walk("/home/omega", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		if info.IsDir() {
+			err = watcher.Add(path)
+			if err != nil {
+				fmt.Println(err)
+				return nil
+			}
+			fmt.Println("Directory added:", path)
+		} else {
+			err = watcher.Add(filepath.Dir(path))
+			if err != nil {
+				fmt.Println(err)
+				return nil
+			}
+			fmt.Println("Directory added:", filepath.Dir(path))
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-  for {
-        select{
-    case event,ok := <-watcher.Events:
-    if !ok {
-      return
-    }
-    if event.Op&fsnotify.Write == fsnotify.Write {
-     fmt.Println("Fichier modifié", event.Name) 
-    }
-    case err,ok := <- watcher.Errors:
-    if !ok {
-      return
-    }
-    fmt.Println("Erreur de surveillance:",err)
-  }
-  }
-
-
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+			if event.Op&fsnotify.Write == fsnotify.Write {
+				fmt.Println("File modified:", event.Name)
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			fmt.Println("Watch error:", err)
+		}
+	}
 }
